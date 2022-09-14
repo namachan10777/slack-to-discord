e0adc2560c4a7dbc04c7b88c5e78f4f9f25e46a8
Masaki Nakano <admin@namachan10777.dev>
2022-09-14 19:05:36 +0900

    show zip entries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use clap::Parser;
+use std::path::PathBuf;
+use std::{fs, io};
+
+#[derive(clap::Parser, Debug)]
+struct Opts {
+    #[clap(short, long)]
+    msg: PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opts = Opts::parse();
+    let file = fs::File::open(opts.msg).with_context(|| "Reading msg archive")?;
+    let file = io::BufReader::new(file);
+    let mut archive = zip::ZipArchive::new(file).with_context(|| "Open msg archive")?;
+    for idx in 0..archive.len() {
+        let file = archive.by_index(idx).with_context(|| "Reading msg file")?;
+        println!("{:?}", String::from_utf8_lossy(file.name_raw()));
+    }
+    Ok(())
 }
